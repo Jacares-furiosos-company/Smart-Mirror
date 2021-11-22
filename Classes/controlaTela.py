d9599f1fd21bcf6ccc61c873628dd9fcb2fd23ee
Francis Junior <francisdaluznv@gmail.com>
2021-11-22 18:41:29 -0300

    Atualização da função de apagar a tela

--- Classes/controlaTela.py
+++ Classes/controlaTela.py
@@ -1,10 +1,13 @@
 import screen_brightness_control as sbc
+import time
 
-distanciaEncontrada = 40 #dis.verificaDistancia()
+while True:
+    distanciaEncontrada = dis.verificaDistancia()
 
-if (distanciaEncontrada <= 40):
-    sbc.set_brightness(100)
-elif (distanciaEncontrada > 40):
-    sbc.set_brightness(1)
+    if (distanciaEncontrada <= 40):
+        sbc.set_brightness(100)
+    elif (distanciaEncontrada > 40):
+        sbc.set_brightness(1)
+    time.sleep(3)
 
-print(sbc.get_brightness())
+
