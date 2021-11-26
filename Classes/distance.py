817d341ed8f59b1dc351a8a421673312f65e221f
Francis Junior <francisdaluznv@gmail.com>
2021-11-26 16:15:01 -0300

    distancia

--- Classes/distance.py
+++ Classes/distance.py
@@ -2,37 +2,35 @@ import subprocess
 import RPi.GPIO as GPIO
 import time
 
+while True:
+    try:
+        GPIO.setmode(GPIO.BOARD)
+        pinTrigger = 18
+        pinEcho = 16
 
-def verificaDistancia():
-    while True:
-        try:
-            GPIO.setmode(GPIO.BOARD)
-            pinTrigger = 18
-            pinEcho = 16
+        GPIO.setup(pinTrigger, GPIO.OUT)
+        GPIO.setup(pinEcho, GPIO.IN)
 
-            GPIO.setup(pinTrigger, GPIO.OUT)
-            GPIO.setup(pinEcho, GPIO.IN)
+        GPIO.output(pinTrigger, GPIO.LOW)
+        GPIO.output(pinTrigger, GPIO.HIGH)
 
-            GPIO.output(pinTrigger, GPIO.LOW)
-            GPIO.output(pinTrigger, GPIO.HIGH)
+        GPIO.output(pinTrigger, GPIO.LOW)
 
-            GPIO.output(pinTrigger, GPIO.LOW)
+        while GPIO.input(pinEcho) == 0:
+            pulseStartTime = time.time()
+        while GPIO.input(pinEcho) == 1:
+            pulseEndTime = time.time()
 
-            while GPIO.input(pinEcho) == 0:
-                pulseStartTime = time.time()
-            while GPIO.input(pinEcho) == 1:
-                pulseEndTime = time.time()
+        pulseDuration = pulseEndTime - pulseStartTime
+        distance = round(pulseDuration * 17150, 2)
 
-            pulseDuration = pulseEndTime - pulseStartTime
-            distance = round(pulseDuration * 17150, 2)
+        print("Distance: %.2f cm" % (distance))
 
-            print("Distance: %.2f cm" % (distance))
+        if (distance <= 40):
+            subprocess.run(['vcgencmd display_power 1'])
+        elif (distance > 40):
+            subprocess.run(['vcgencmd display_power 0'])
 
-            if (distance <= 40):
-                subprocess.run(['vcgencmd display_power 1'])
-            elif (distance > 40):
-                subprocess.run(['vcgencmd display_power 0'])
-
-        finally:
-            GPIO.cleanup()
-            time.sleep(3)
+    finally:
+        GPIO.cleanup()
+        time.sleep(3)
